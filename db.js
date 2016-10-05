aeb1d56ebdc936d58309e0a030ead02343e7afed
Doug Wight <dcfwight@gmail.com>
2016-10-05 10:56:34 +0100

    Add postgres for production

--- db.js
+++ db.js
@@ -1,10 +1,26 @@
-// creates a new sql database
 
+
+// creates a new sql database OR POSTGRES database, if we are using Heroku
+// environment variables are set according to the environment we are using.
+// they are all stored in process.env object, and are all strings.
+// so it looks to see if process.env.NODE_ENV exists and is set as production - if it does we are using Heroku, if not, then set as 'development'
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined,undefined,undefined, {
+var sequelize;
+var env = process.env.NODE_ENV || 'development';
+
+
+if (env === 'production') {
+    sequelize = new Sequeulize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    })
+} else {
+    sequelize = new Sequelize(undefined,undefined,undefined, {
     'dialect':'sqlite',
     'storage': __dirname + '/data/dev-todo-api.sqlite' //__dirname is the current folder.
-});
+});    
+}
+
+
 
 var db ={}; // this is common - set db object as the object to return from a database file system.
 
